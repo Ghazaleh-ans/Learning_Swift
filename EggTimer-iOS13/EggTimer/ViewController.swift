79d0a5bc85e25600264bb6181008fb05947ca3fe
Ghazaleh <ghazaaleh.ansari@gmail.com>
2026-02-18 11:54:39 +0100

    challange is done

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -9,38 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let eggTimes = ["Soft": 5, "Medium":7, "Hard": 12]
+    
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
     var counter = 60
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
-    }
-
-
+    var timer: Timer?          // store a reference to the timer
 
     @objc func updateCounter() {
-        //example functionality
         if counter > 0 {
             print("\(counter) seconds.")
             counter -= 1
+        } else {
+            timer?.invalidate()   // stop the timer when it hits 0
+            timer = nil
+            print("Done!")
         }
     }
 
-    
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer?.invalidate()       // cancel any previous timer before starting a new one
+        
         let hardness = sender.currentTitle!
-//        if hardness == "Soft" {
-//            print(eggTimes["Soft"]!)
-//        }
-//        else if hardness == "Medium" {
-//            print(eggTimes["Medium"]!)
-//        }
-//        else{
-//            print(eggTimes["Hard"]!)
-//        }
-        let result = eggTimes[hardness]!
-        print(result)
+        counter = eggTimes[hardness]!   // assign to the INSTANCE variable, not a new local one
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
 }
