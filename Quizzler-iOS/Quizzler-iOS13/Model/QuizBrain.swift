6a3209eb8551757e0d5bcb6ba8d1bafd32eecb89
Ghazaleh <ghazaaleh.ansari@gmail.com>
2026-03-16 18:50:41 +0100

    true-false quiz is done

--- Quizzler-iOS/Quizzler-iOS13/Model/QuizBrain.swift
+++ Quizzler-iOS/Quizzler-iOS13/Model/QuizBrain.swift
@@ -24,8 +24,11 @@ struct QuizBrain {
     ]
     var questionNumber: Int = 0
     
-    func checkAnswer(_ userAnswer: String) -> Bool {
+    var score: Int = 0
+    
+    mutating func checkAnswer(_ userAnswer: String) -> Bool {
         if userAnswer == quiz[questionNumber].answer {
+            score += 1
             return true
         } else {
             return false
@@ -44,9 +47,14 @@ struct QuizBrain {
     mutating func nextQuestion() {
         if questionNumber == quiz.count - 1 {
             questionNumber = 0
+            score = 0
         }
         else{
             questionNumber += 1
         }
+    }
+    
+    func getScore() -> Int {
+        return score
     }
 }
