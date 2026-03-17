5aa8f77d79e7d130f56b55f150e0ef7094fbe3e4
Ghazaleh <ghazaaleh.ansari@gmail.com>
2026-03-17 15:05:47 +0100

    Stories dictionary has been created by our own struct

--- Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
@@ -14,6 +14,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let stories: [Story] = [Story(title: "You see a fork in the road.", choice1: "Take a left", choice2: "Take a right"),
+                            Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+                            Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
+    ]
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
