e60f9c18ace0a81bb6ac54cb44d90db9e37d0e82
Ghazaleh <ghazaaleh.ansari@gmail.com>
2026-03-26 19:32:31 +0100

    sliders works

--- BMI-Calculator-iOS13-master/BMI Calculator/Controllers/ViewController.swift
+++ BMI-Calculator-iOS13-master/BMI Calculator/Controllers/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var heightValue: UILabel!
+    @IBOutlet weak var weightValue: UILabel!
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let height = ((sender.value) * 100).rounded() / 100
+        heightValue.text = "\(height)m"
+        print(height)
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let weight = ((sender.value) * 100).rounded() / 100
+        weightValue.text = "\(weight)Kg"
+        print(weight)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
