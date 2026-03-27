63c7c23786c9fd95e3298c0ae9ef104a83bf69c7
Ghazaleh <ghazaaleh.ansari@gmail.com>
2026-03-27 13:07:47 +0100

    override method from super class

--- ClassesDemo/ClassesDemo/Dragon.swift
+++ ClassesDemo/ClassesDemo/Dragon.swift
@@ -1,4 +1,12 @@
 
 class Dragon: Enemy {
+    var wingSpan = 2
     
+    func talk(_ speech: String) {
+        print("Says: \(speech)")
+    }
+    
+    override func move() {
+        print("Fly forward!")
+    }
 }
--- ClassesDemo/ClassesDemo/main.swift
+++ ClassesDemo/ClassesDemo/main.swift
@@ -5,6 +5,9 @@ skeleton.move()
 skeleton.attack()
 
 let dragon = Dragon()
+dragon.wingSpan = 5
+dragon.attackStrength = 20
+dragon.talk("Roar!")
 print(dragon.health)
 dragon.move()
 dragon.attack()
