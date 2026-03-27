0afe00fb6fd70f674e664de3ecb793839c35e551
Ghazaleh <ghazaaleh.ansari@gmail.com>
2026-03-27 14:15:03 +0100

    create our own ViewController

---
+++ BMI-Calculator-iOS13-master/BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Ghazall on 27.03.26.
+//  Copyright © 2026 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
