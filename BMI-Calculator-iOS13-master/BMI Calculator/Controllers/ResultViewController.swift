5094aac298e08adcb01d4471f4143e4b2159da6f
Ghazaleh <ghazaaleh.ansari@gmail.com>
2026-03-27 20:30:47 +0100

    create viewController

---
+++ BMI-Calculator-iOS13-master/BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by Ghazall on 27.03.26.
+//  Copyright © 2026 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+}
