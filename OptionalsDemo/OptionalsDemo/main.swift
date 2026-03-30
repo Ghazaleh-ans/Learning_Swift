9ec7c821150f25adcdc480c4a3c8e36f735da111
Ghazaleh <ghazaaleh.ansari@gmail.com>
2026-03-30 17:27:43 +0200

    force unwraping

---
+++ OptionalsDemo/OptionalsDemo/main.swift
@@ -0,0 +1,8 @@
+
+let myOptional: String?
+
+myOptional = "Angela"
+
+//Force Unwarping the optional -> least safe way
+// if myOptioanal be nil we will have runtime error
+let text: String = myOptional!
