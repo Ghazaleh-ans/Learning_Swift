b50f63bcc915fbe3bad4a4de856d35e8a193549e
Ghazaleh <ghazaaleh.ansari@gmail.com>
2026-03-30 17:31:28 +0200

    check for optional

--- OptionalsDemo/OptionalsDemo/main.swift
+++ OptionalsDemo/OptionalsDemo/main.swift
@@ -3,6 +3,13 @@ let myOptional: String?
 
 myOptional = "Angela"
 
-//Force Unwarping the optional -> least safe way
+//1.Force Unwarping the optional -> least safe way
 // if myOptioanal be nil we will have runtime error
-let text: String = myOptional!
+//let text: String = myOptional!
+
+//2.Check for nil value
+if myOptional != nil {
+    let text: String = myOptional!
+} else {
+    print("myOptional was found to be nil.")
+}
